42db53e4a65f21b093e90b5186d52a52bb0c54b8
Diaz <diyaz9131@gmail.com>
2024-02-02 16:06:46 +0500

    update 02.02.24

--- html_parser.py
+++ html_parser.py
@@ -2,14 +2,11 @@ import mammoth
 
 custom_styles = "b => i" 
 
-# Открываем документ Word
 with open("PriceList.docx", 'rb') as docx_file:
     result = mammoth.convert_to_html(docx_file, style_map = custom_styles) 
 
-# Получаем HTML из результата
 html = result.value
 
-# Выводим HTML
 print(html)
 
 
